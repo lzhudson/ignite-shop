2476d0eb6196a481adadff5922c8e1d7a4080740
Hudson Holanda <hudsonholanda55@gmail.com>
2024-06-25 07:42:28 -0300

    feat: wrap application with CartProvider and use Header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,25 @@
 import { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
-import logoImg from '../assets/logo.svg'
-import { Container, Header } from '../styles/pages/app'
-import Image from 'next/image'
+
+import { CartProvider } from 'use-shopping-cart'
+import { Container } from '../styles/pages/app'
+import { Header } from '../components/Header'
+
 globalStyles()
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Container>
-      <Header>
-        <Image src={logoImg} alt="" />
-      </Header>
-      <Component {...pageProps} />
-    </Container>
+    <CartProvider
+      cartMode="checkout-session"
+      stripe={process.env.STRIPE_SECRET_KEY}
+      currency="BRL"
+      language="pt-BR"
+      loading={<p>Loading</p>}
+      shouldPersist={true}
+    >
+      <Container>
+        <Header />
+        <Component {...pageProps} />
+      </Container>
+    </CartProvider>
   )
 }
