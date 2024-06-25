fca6965ccc81f824e021513af2f2db496b3a5d43
Hudson Holanda <hudsonholanda55@gmail.com>
2024-06-25 07:43:41 -0300

    feat: modify api to accept multiples itens to checkout

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -5,19 +5,30 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const { priceId } = req.body
-  console.log(req.body)
+  const { products } = req.body
+
   if (req.method !== 'POST') {
     return res.status(405).json({
       error: 'Method not allowd.',
     })
   }
+  const hasPriceIdPropInCart = products.every(product => {
+    const hasPriceId = 'priceId' in product;
+    return hasPriceId
+  })
 
-  if (!priceId) {
+  if (!hasPriceIdPropInCart) {
     return res.status(400).json({
       error: 'Price not found.',
     })
   }
+
+  const cartFormatted = products.map(product => {
+    return {
+      price: product.priceId,
+      quantity: product.quantity
+    }
+  })
 
   const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
   const cancelUrl = `${process.env.NEXT_URL}/`
@@ -25,12 +36,7 @@ export default async function handler(
     success_url: successUrl,
     cancel_url: cancelUrl,
     mode: 'payment',
-    line_items: [
-      {
-        price: priceId,
-        quantity: 1,
-      },
-    ],
+    line_items: cartFormatted
   })
 
   return res.status(201).json({
